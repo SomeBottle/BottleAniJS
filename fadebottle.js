8c5e7b833ea846d3d1a0bfe71fa6b541578cca58
SomeBottle <583797659@qq.com>
2018-04-22 11:49:59 +0800

    Fadebottle淡入淡出

---
+++ fadebottle.js
@@ -0,0 +1,46 @@
+﻿function FadeIn(e,t){
+	//t=100 = 1秒
+	var m=document.getElementById(e);
+	m.style.opacity=0;
+	var g=1/t;
+	var timer;
+	function fi(){
+	m.style.opacity=Number(m.style.opacity)+g;
+	if(Number(m.style.opacity)>=1){
+		clearInterval(timer);
+	}
+	}
+	timer=setInterval(fi,1);
+}
+function FadeOut(e,t){
+	//t=100 = 1秒
+	var m=document.getElementById(e);
+	m.style.opacity=1;
+	var g=1/t;
+	var timer;
+	function fi(){
+	m.style.opacity=Number(m.style.opacity)-g;
+	if(Number(m.style.opacity)<=0){
+		clearInterval(timer);
+	}
+	}
+	timer=setInterval(fi,1);
+}
+function FadeInAndOut(e,t1,t2,tw){ //(元素，渐入时间，渐出时间，中间停留时间)
+//渐入
+	var m=document.getElementById(e);
+	m.style.opacity=0;
+	var g=1/t1;
+	var timer;
+	function fi(){
+	m.style.opacity=Number(m.style.opacity)+g;
+	if(Number(m.style.opacity)>=1){
+		clearInterval(timer);
+		function fo(){
+			FadeOut(e,t2);
+		}
+		setTimeout(fo,tw*10);
+	}
+	}
+	timer=setInterval(fi,1);
+}
